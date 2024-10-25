67377d5f04e302a47c2afa16869acce68563e85f
Veyasan1 <86246566+Veyasan1@users.noreply.github.com>
2024-10-25 12:06:10 -0400

    Uploaded progress as of 25/10/2024
    Progress just before agreeing to collaborate on project. Parts of the introduction, abstract, and data and model sections are completed, along with a rudimentary model based upon pollster data predicting a Harris win.

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,37 +1,95 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models the 2024 US presidential poll data based on who would vote for Harris
+# Author: Veyasan Ragulan
+# Date: 18 October 2023
+# Contact: veyasan.ragulan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: president_polls.csv, which is the presidental general election poll sourced from 538
 
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
+library(lubridate)
+library(broom)
+library(modelsummary)
 library(rstanarm)
+library(splines)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_csv("data/02-analysis_data/harris.csv")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+#### Bayesian models ####
+# Change 'pollster' and 'state' to factor variables
+analysis_data <- analysis_data |>
+  mutate(
+    pollster = factor(pollster),
+    state = factor(state),
+    sponsors = factor(sponsors),
+    methodology = factor(methodology)
   )
 
+# Model 
+model_formula <- cbind(num_harris, sample_size - num_harris) ~ (1 | pollster) + (1 | state) + (1 | sponsors) + (1 | methodology)
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+# Specify priors
+priors <- normal(0, 2.5, autoscale = TRUE)
+
+# Fit the models
+bayesian_model <- stan_glmer(
+  formula = model_formula,
+  data = analysis_data,
+  family = binomial(link = "logit"),
+  prior = priors,
+  prior_intercept = priors,
+  seed = 123,
+  cores = 4,
+  adapt_delta = 0.95
 )
 
+# Posterior predictive checks
+pp_check(bayesian_model)
+
+# Summarize the model
+summary(bayesian_model)
+
+# Plot random effects
+plot(bayesian_model, pars = "(Intercept)", prob = 0.95)
+
+#### Bayesian models and splines ####
+# Change date to be number of days since she declared - it's a counter not a date
+analysis_data <- analysis_data |>
+  mutate(
+    end_date_num = as.numeric(end_date - min(end_date))
+  )
+
+# Fit Bayesian model with spline and pollster as fixed effect
+spline_model <- stan_glm(
+  pct ~ ns(end_date_num, df = 5) + pollster + state + sponsors + methodology, # Change df for the number of "bits" - higher numbers - more "wiggly" - but then need to worry about overfitting.
+  data = analysis_data,
+  family = gaussian(),
+  prior = normal(0, 5),
+  prior_intercept = normal(50, 10),
+  seed = 1234,
+  iter = 2000,
+  chains = 4,
+  refresh = 0
+)
+
+# Summarize the model
+summary(spline_model)
+
+# Posterior predictive checks
+pp_check(spline_model)
+
+# Save model
+saveRDS(
+  bayesian_model,
+  file = "models/electionmodel.rds"
+)
+
+saveRDS(
+  spline_model,
+  file = "models/splinemodel.rds"
+)
 
