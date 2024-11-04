f4236dc06633723714dc220f636414ab00335347
prankitbhardwaj <prankit.bhardwaj@mail.utoronto.ca>
2024-11-04 16:56:55 -0500

    Update 06-model_data.R

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -2,7 +2,7 @@
 # Purpose: Linear Models for Prediction
 # Author: Prankit Bhardwaj, Veyasan Ragulan, Chris Yong Hong Sen
 # Date: 4 November 2024
-# Contact: prankit.bhardwaj@mail.utoronto.ca [...UPDATE THIS...]
+# Contact: prankit.bhardwaj@mail.utoronto.ca 
 # License: MIT
 
 
